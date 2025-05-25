03b317e135247adc194ef3c8ee2b7abe1ad6cc9a
PfoRsten <marvin-hauke@hotmail.de>
2025-05-26 01:44:00 +0200

    added bufferclose function

--- lua/tickets/init.lua
+++ lua/tickets/init.lua
@@ -37,6 +37,18 @@ local function open_floating_file(target_file)
     vim.bo[buf].swapfile = false
 
     local win = vim.api.nvim_open_win(buf, true, win_config())
+
+    vim.api.nvim_buf_set_keymap(buf, "n", "q", "", {
+        noremap = true,
+        silent = true,
+        callback = function()
+            if vim.api.nvim_get_option_value("modified", { buf = buf }) then
+                vim.notify("save your changes pls", vim.log.levels.WARN)
+            else
+                vim.api.nvim_win_close(win, true)
+            end
+        end,
+    })
 end
 
 local function setup_user_commands(opts)
