9cf785947e186e84cead03b2f727bf721ef3a98c
PfoRsten <marvin-hauke@hotmail.de>
2025-05-27 01:15:52 +0200

    added q to close the window

--- lua/tickets/init.lua
+++ lua/tickets/init.lua
@@ -1,12 +1,26 @@
 local M = {}
 
 local ui = require("tickets.ui")
+local utils = require("tickets.utils")
 
 local function setup_user_commands(opts)
     local target_file = opts.target_file or "todo.md"
     vim.api.nvim_create_user_command("Td", function()
         ui.open_floating_file(target_file)
     end, {})
+
+    local buf = utils.get_or_create_buf(target_file)
+    vim.api.nvim_buf_set_keymap(buf, "n", "q", "", {
+        noremap = true,
+        silent = true,
+        callback = function()
+            if vim.api.nvim_get_option_value("modified", { buf = buf }) then
+                vim.notify("save your changes pls", vim.log.levels.WARN)
+            else
+                vim.api.nvim_win_close(0, true)
+            end
+        end,
+    })
 end
 
 M.setup = function(opts)
--- lua/tickets/utils.lua
+++ lua/tickets/utils.lua
@@ -1,5 +1,18 @@
 M = {}
 
+function M.get_or_create_buf(path)
+    local expanded_path = M.expand_path(path) -- reuse your existing expand_path
+
+    local buf = vim.fn.bufnr(expanded_path, true)
+    if buf == -1 then
+        buf = vim.api.nvim_create_buf(false, false)
+        vim.api.nvim_buf_set_name(buf, expanded_path)
+    end
+
+    vim.bo[buf].swapfile = false
+    return buf
+end
+
 function M.expand_path(path)
     if path:sub(1, 1) == "~" then
         return os.getenv("HOME") .. path:sub(2)
